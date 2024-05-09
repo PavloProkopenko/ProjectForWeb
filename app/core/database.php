483d29dd5e72fa91e9b51d7290bdf9bfd21d6714
Pavlo Prokopenko <stpasha6@gmail.com>
2024-05-09 22:58:58 +0300

    Making query for database

--- app/core/database.php
+++ app/core/database.php
@@ -8,14 +8,36 @@ class Database
 {
 	private function connect()
     {
-        $str = 'mysql:host=localhost;dbname=courses_db';
-        $con = new PDO($str, 'root', '');
+        $str = DB_DRIVER.":hostname=".DB_HOST.";dbname=".DB_NAME;
+        return new PDO($str, 'root', '');
     }
 
-    public function query()
+    public function query($query, $data = array(), $type = 'object')
     {
         $con = $this->connect();
-        var_dump($con);
+
+        $stm = $con->prepare($query);
+        if($stm)
+        {
+            $check = $stm->execute($data);
+            if($check)
+            {
+                $type = PDO::FETCH_OBJ;
+                if($type != 'object')
+                {
+                    $type = PDO::FETCH_ASSOC;
+                }
+
+                $result = $stm->fetchAll($type);
+
+                if($result && count($result) > 0)
+                {
+                    return $result;
+                }
+            }
+        }
+
+        return false;
     }
 	
 }
