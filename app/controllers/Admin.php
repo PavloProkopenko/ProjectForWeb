a2d2b980dc5bc1cba3fde1656f32344c047cd3c7
Pavlo Prokopenko <stpasha6@gmail.com>
2024-05-11 21:35:39 +0300

    Adding admin dashboard

---
+++ app/controllers/Admin.php
@@ -0,0 +1,17 @@
+<?php 
+
+/**
+ * admin class
+ */
+class Admin extends Controller
+{
+	
+	public function index()
+	{
+
+		$data['title'] = "Admin";
+
+		$this->view('admin/dashboard',$data);
+	}
+	
+}
